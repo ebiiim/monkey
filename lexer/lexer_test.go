9a69e29f6a1cd647f163a2ec6990b4fa0704fd87
ebiiim <mail@ebiiim.com>
2020-09-25 17:38:34 +0900

    refactor lexer_test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,115 +7,93 @@ import (
 	"github.com/ebiiim/monkey/token"
 )
 
-func TestNextToken1(t *testing.T) {
-	input := `=+(){},;`
-
-	tests := []struct {
-		wantType         token.Type
-		wantLiteral      string
-		wantRow, wantCol int
+func TestNextToken(t *testing.T) {
+	cases := []struct {
+		name       string
+		input      string
+		wantTokens []token.Token
 	}{
-		{token.ASSIGN, "=", 1, 1},
-		{token.PLUS, "+", 1, 2},
-		{token.LPAREN, "(", 1, 3},
-		{token.RPAREN, ")", 1, 4},
-		{token.LBRACE, "{", 1, 5},
-		{token.RBRACE, "}", 1, 6},
-		{token.COMMA, ",", 1, 7},
-		{token.SEMICOLON, ";", 1, 8},
-		{token.EOF, "", 1, 9},
-	}
-
-	l := lexer.New(input)
-	for i, tt := range tests {
-		tok := l.NextToken()
-		if tok.Type != tt.wantType {
-			t.Fatalf("test %d: wrong token.Type want=%v got=%v", i, tt.wantType, tok.Type)
-		}
-		if tok.Literal != tt.wantLiteral {
-			t.Fatalf("test %d: wrong token.Literal want=%v got=%v", i, tt.wantLiteral, tok.Literal)
-		}
-		if tok.Row != tt.wantRow {
-			t.Fatalf("test %d: wrong token.Row want=%v got=%v", i, tt.wantRow, tok.Row)
-		}
-		if tok.Col != tt.wantCol {
-			t.Fatalf("test %d: wrong token.Col want=%v got=%v", i, tt.wantCol, tok.Col)
-		}
-	}
-}
-
-func TestNextToken2(t *testing.T) {
-	input := `let five = 5;
+		{"1", `=+(){},;`, []token.Token{
+			token.NewS(token.ASSIGN, "=", 1, 1),
+			token.NewS(token.PLUS, "+", 1, 2),
+			token.NewS(token.LPAREN, "(", 1, 3),
+			token.NewS(token.RPAREN, ")", 1, 4),
+			token.NewS(token.LBRACE, "{", 1, 5),
+			token.NewS(token.RBRACE, "}", 1, 6),
+			token.NewS(token.COMMA, ",", 1, 7),
+			token.NewS(token.SEMICOLON, ";", 1, 8),
+			token.NewS(token.EOF, "", 1, 9),
+		}},
+		{"2", `let five = 5;
 let ten = 10;
-	
+
 let add = fn(x, y) {
 	x + y;
 };
-	
-let result = add(five, ten);`
 
-	tests := []struct {
-		wantType         token.Type
-		wantLiteral      string
-		wantRow, wantCol int
-	}{
-		{token.LET, "let", 1, 1},
-		{token.IDENT, "five", 1, 5},
-		{token.ASSIGN, "=", 1, 10},
-		{token.INT, "5", 1, 12},
-		{token.SEMICOLON, ";", 1, 13},
+let result = add(five, ten);`, []token.Token{
+			token.NewS(token.LET, "let", 1, 1),
+			token.NewS(token.IDENT, "five", 1, 5),
+			token.NewS(token.ASSIGN, "=", 1, 10),
+			token.NewS(token.INT, "5", 1, 12),
+			token.NewS(token.SEMICOLON, ";", 1, 13),
 
-		{token.LET, "let", 2, 1},
-		{token.IDENT, "ten", 2, 5},
-		{token.ASSIGN, "=", 2, 9},
-		{token.INT, "10", 2, 11},
-		{token.SEMICOLON, ";", 2, 13},
+			token.NewS(token.LET, "let", 2, 1),
+			token.NewS(token.IDENT, "ten", 2, 5),
+			token.NewS(token.ASSIGN, "=", 2, 9),
+			token.NewS(token.INT, "10", 2, 11),
+			token.NewS(token.SEMICOLON, ";", 2, 13),
 
-		{token.LET, "let", 4, 1},
-		{token.IDENT, "add", 4, 5},
-		{token.ASSIGN, "=", 4, 9},
-		{token.FUNCTION, "fn", 4, 11},
-		{token.LPAREN, "(", 4, 13},
-		{token.IDENT, "x", 4, 14},
-		{token.COMMA, ",", 4, 15},
-		{token.IDENT, "y", 4, 17},
-		{token.RPAREN, ")", 4, 18},
-		{token.LBRACE, "{", 4, 20},
-		{token.IDENT, "x", 5, 5},
-		{token.PLUS, "+", 5, 7},
-		{token.IDENT, "y", 5, 9},
-		{token.SEMICOLON, ";", 5, 10},
-		{token.RBRACE, "}", 6, 1},
-		{token.SEMICOLON, ";", 6, 2},
+			token.NewS(token.LET, "let", 4, 1),
+			token.NewS(token.IDENT, "add", 4, 5),
+			token.NewS(token.ASSIGN, "=", 4, 9),
+			token.NewS(token.FUNCTION, "fn", 4, 11),
+			token.NewS(token.LPAREN, "(", 4, 13),
+			token.NewS(token.IDENT, "x", 4, 14),
+			token.NewS(token.COMMA, ",", 4, 15),
+			token.NewS(token.IDENT, "y", 4, 17),
+			token.NewS(token.RPAREN, ")", 4, 18),
+			token.NewS(token.LBRACE, "{", 4, 20),
+			token.NewS(token.IDENT, "x", 5, 5),
+			token.NewS(token.PLUS, "+", 5, 7),
+			token.NewS(token.IDENT, "y", 5, 9),
+			token.NewS(token.SEMICOLON, ";", 5, 10),
+			token.NewS(token.RBRACE, "}", 6, 1),
+			token.NewS(token.SEMICOLON, ";", 6, 2),
 
-		{token.LET, "let", 8, 1},
-		{token.IDENT, "result", 8, 5},
-		{token.ASSIGN, "=", 8, 12},
-		{token.IDENT, "add", 8, 14},
-		{token.LPAREN, "(", 8, 17},
-		{token.IDENT, "five", 8, 18},
-		{token.COMMA, ",", 8, 22},
-		{token.IDENT, "ten", 8, 24},
-		{token.RPAREN, ")", 8, 27},
-		{token.SEMICOLON, ";", 8, 28},
+			token.NewS(token.LET, "let", 8, 1),
+			token.NewS(token.IDENT, "result", 8, 5),
+			token.NewS(token.ASSIGN, "=", 8, 12),
+			token.NewS(token.IDENT, "add", 8, 14),
+			token.NewS(token.LPAREN, "(", 8, 17),
+			token.NewS(token.IDENT, "five", 8, 18),
+			token.NewS(token.COMMA, ",", 8, 22),
+			token.NewS(token.IDENT, "ten", 8, 24),
+			token.NewS(token.RPAREN, ")", 8, 27),
+			token.NewS(token.SEMICOLON, ";", 8, 28),
 
-		{token.EOF, "", 8, 29},
+			token.NewS(token.EOF, "", 8, 29),
+		}},
 	}
-
-	l := lexer.New(input)
-	for i, tt := range tests {
-		tok := l.NextToken()
-		if tok.Type != tt.wantType {
-			t.Fatalf("test %d: wrong token.Type want=%v got=%v", i, tt.wantType, tok.Type)
-		}
-		if tok.Literal != tt.wantLiteral {
-			t.Fatalf("test %d: wrong token.Literal want=%v got=%v", i, tt.wantLiteral, tok.Literal)
-		}
-		if tok.Row != tt.wantRow {
-			t.Fatalf("test %d: wrong token.Row want=%v got=%v", i, tt.wantRow, tok.Row)
-		}
-		if tok.Col != tt.wantCol {
-			t.Fatalf("test %d: wrong token.Col want=%v got=%v", i, tt.wantCol, tok.Col)
-		}
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			l := lexer.New(c.input)
+			for i, wt := range c.wantTokens {
+				tok := l.NextToken()
+				if tok.Type != wt.Type {
+					t.Fatalf("token#%d: wrong token.Type want=%v got=%v", i, wt.Type, tok.Type)
+				}
+				if tok.Literal != wt.Literal {
+					t.Fatalf("token#%d: wrong token.Literal want=%v got=%v", i, wt.Literal, tok.Literal)
+				}
+				if tok.Row != wt.Row {
+					t.Fatalf("token#%d: wrong token.Row want=%v got=%v", i, wt.Row, tok.Row)
+				}
+				if tok.Col != wt.Col {
+					t.Fatalf("token#%d: wrong token.Col want=%v got=%v", i, wt.Col, tok.Col)
+				}
+			}
+		})
 	}
 }
