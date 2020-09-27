588ac5b4b51df28b63cd695e6461c7f4b25f9f4f
ebiiim <mail@ebiiim.com>
2020-09-28 02:51:37 +0900

    eval infix (section 3.5.6); now we can do 1+2+3

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -29,6 +29,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpressions(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpressions(node.Operator, left, right)
 	}
 	return nil
 }
@@ -70,6 +74,45 @@ func evalMinusOperatorExpression(right object.Object) object.Object {
 		return NULL
 	}
 	return &object.Integer{Value: -right.(*object.Integer).Value}
+}
+
+func evalInfixExpressions(op string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(op, left, right)
+	// compare memory addresses because we have just one TRUE and FALSE
+	case op == token.EQ:
+		return nativeBoolToBooleanObject(left == right)
+	case op == token.NEQ:
+		return nativeBoolToBooleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(op string, left, right object.Object) object.Object {
+	l := left.(*object.Integer).Value
+	r := right.(*object.Integer).Value
+	switch op {
+	case token.PLUS:
+		return &object.Integer{Value: l + r}
+	case token.MINUS:
+		return &object.Integer{Value: l - r}
+	case token.ASTERISK:
+		return &object.Integer{Value: l * r}
+	case token.SLASH:
+		return &object.Integer{Value: l / r}
+	case token.LT:
+		return nativeBoolToBooleanObject(l < r)
+	case token.GT:
+		return nativeBoolToBooleanObject(l > r)
+	case token.EQ:
+		return nativeBoolToBooleanObject(l == r)
+	case token.NEQ:
+		return nativeBoolToBooleanObject(l != r)
+	default:
+		return NULL
+	}
 }
 
 func nativeBoolToBooleanObject(v bool) object.Object {
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -18,6 +18,16 @@ func TestEvalIntegerExpression(t *testing.T) {
 		{"10", 10},
 		{"-5", -5},
 		{"-10", -10},
+		{"5 + 5 + 5 + 5 - 10", 5 + 5 + 5 + 5 - 10},
+		{"2 * 2 * 2 * 2 * 2", 2 * 2 * 2 * 2 * 2},
+		{"5 * 2 + 10", 5*2 + 10},
+		{"5 + 10 * 10", 5 + 10*10},
+		{"20 + 2 * -10", 20 + 2*-10},
+		{"50 / 2 * 2 + 10", 50/2*2 + 10},
+		{"2 * (5 + 10)", 2 * (5 + 10)},
+		{"3 * 3 * 3 + 10", 3*3*3 + 10},
+		{"3 * (3 * 3) + 10", 3*(3*3) + 10},
+		{"(5 + 10 * 2 + 15 / 3) * 2 - 10", (5+10*2+15/3)*2 - 10},
 	}
 	for _, c := range cases {
 		c := c
@@ -35,6 +45,23 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"true", true},
 		{"false", false},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
 	}
 	for _, c := range cases {
 		c := c
